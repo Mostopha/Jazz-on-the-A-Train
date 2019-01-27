25899a765bd1a2d3330134f13bc4b85d6a7ea9dd
Tira Odhner <atiraodhner@gmail.com>
2019-01-26 19:25:51 -0500

    Remove dead code

--- GGJ_JazzOnTheATrain/Assets/Scripts/SceneChanger.cs
+++ GGJ_JazzOnTheATrain/Assets/Scripts/SceneChanger.cs
@@ -10,7 +10,6 @@ public class SceneChanger : MonoBehaviour
 {
     public static SceneChanger sceneChanger;
 
-    private Room room;
 
     // Start is called before the first frame update
     void Start()
