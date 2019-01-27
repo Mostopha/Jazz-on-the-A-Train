0b4c2ded71b6ee5473fa5b3ea2f2bb3e268ba56b
Andrew Petersell <andrewpetersell@gmail.com>
2019-01-27 03:59:27 -0500

    Character Animations

---
+++ GGJ_JazzOnTheATrain/Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetTrigger("Idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("Talking");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("Jamming");
+        }
+    }
+}
