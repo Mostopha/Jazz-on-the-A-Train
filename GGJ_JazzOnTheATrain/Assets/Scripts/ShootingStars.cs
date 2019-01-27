b722b8bbfbaad0ccc7cfb6665a98c2ea391a465c
Mostafa Haque <93haque@gmail.com>
2019-01-27 02:54:00 -0500

    Speeding train background

---
+++ GGJ_JazzOnTheATrain/Assets/Scripts/ShootingStars.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingStars : MonoBehaviour
+{
+    public Material starField;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //starField = this.GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        starField.mainTextureOffset = new Vector2((starField.mainTextureOffset.x + Time.deltaTime), 1);
+    }
+    
+}
