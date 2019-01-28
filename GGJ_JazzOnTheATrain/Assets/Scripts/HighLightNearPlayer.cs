9d87fb37cf1ef2c11c27f6c58daa1f9fec7cb8ee
Mostafa Haque <93haque@gmail.com>
2019-01-27 23:51:54 -0500

    End and beginning scenes working. Button prompt. Main Menu

--- GGJ_JazzOnTheATrain/Assets/Scripts/HighLightNearPlayer.cs
+++ GGJ_JazzOnTheATrain/Assets/Scripts/HighLightNearPlayer.cs
@@ -9,6 +9,8 @@ public class HighLightNearPlayer : MonoBehaviour
     public float distance;
 
     public Vector3 originalScale;
+
+    public bool tutorial;
     
     
     // Start is called before the first frame update
@@ -30,12 +32,30 @@ public class HighLightNearPlayer : MonoBehaviour
         }
         else
         {
-            this.transform.localScale = originalScale;
+            //this.transform.localScale = originalScale;
+            SpriteRenderer[] _sprites = this.GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer _localSprite in _sprites)
+            {
+                _localSprite.color = Color.white;
+                
+                SpriteRenderer _spaceBar = GameObject.Find("SpaceBar").GetComponent<SpriteRenderer>();
+                _spaceBar.enabled = false;
+            }
+
         }
     }
 
     void Bigger()
     {
-        this.transform.localScale = new Vector3(1.2f,1.2f,1.2f);
+       // this.transform.localScale = new Vector3(1.2f,1.2f,1.2f);
+
+        SpriteRenderer[] _sprites = this.GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer _localSprite in _sprites)
+        {
+            _localSprite.color = Color.red;
+            SpriteRenderer _spaceBar = GameObject.Find("SpaceBar").GetComponent<SpriteRenderer>();
+            _spaceBar.enabled = true;
+        }
+
     }
 }
